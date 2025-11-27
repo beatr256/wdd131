0be348dcd85007f3ca22e422424af58260e10f6c
beatr256 <hapolot@byupathway.edu>
2025-11-27 01:24:57 -0800

    push

--- scripts/form.js
+++ scripts/form.js
@@ -1,15 +1,16 @@
-// Populate dropdown
+// Populate product select options
 const products = [
-  "Solar Panel X200",
-  "Water Pump Pro",
-  "Battery Pack Lite",
-  "Wind Turbine Mini"
+  {id: "fc-1888", name: "flux capacitor"},
+  {id: "fc-2050", name: "power laces"},
+  {id: "fs-1987", name: "time circuits"},
+  {id: "ac-2000", name: "low voltage reactor"},
+  {id: "jj-1969", name: "warp equalizer"}
 ];
 
 const productSelect = document.getElementById("product");
-products.forEach(item => {
-  const opt = document.createElement("option");
-  opt.value = item;
-  opt.textContent = item;
-  productSelect.appendChild(opt);
+products.forEach(p => {
+  const option = document.createElement("option");
+  option.value = p.id;
+  option.textContent = p.name;
+  productSelect.appendChild(option);
 });
